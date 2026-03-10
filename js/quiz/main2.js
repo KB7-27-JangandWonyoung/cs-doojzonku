cafa1a393511f9a71b562625e7a157eeb8be183d
HwangSeo <sadclara0830@naver.com>
2026-03-10 17:02:27 +0900

    feat : 문제 색깔 상태 css 적용

--- js/quiz/main2.js
+++ js/quiz/main2.js
@@ -89,8 +89,7 @@ function loadQuestion() {
 
     optionButtons.forEach((btn, idx) => {
         btn.querySelector('.option-text').textContent = currentQuiz.options[idx];
-        btn.classList.remove('active');
-        btn.style.backgroundColor = "";
+        btn.classList.remove('active', 'correct', 'wrong', 'disabled'); // 클래스 모두 제거
         btn.disabled = false;
         btn.onclick = () => {
             optionButtons.forEach(b => b.classList.remove('active'));
@@ -144,15 +143,22 @@ confirmBtn.onclick = () => {
     
     optionButtons.forEach((btn, idx) => {
         btn.disabled = true;
-        if (idx === currentQuiz.answer) btn.style.backgroundColor = "#d4edda";
-        else if (idx === selectedOptionIndex && !isCorrect) btn.style.backgroundColor = "#f8d7da";
+        if (idx === currentQuiz.answer) {
+        // 정답인 버튼에 .correct 클래스 추가 (안내 문구 자동 노출)
+        btn.classList.add('correct');
+        } else if (idx === selectedOptionIndex && !isCorrect) {
+        // 내가 선택한게 오답일 때 .wrong 클래스 추가
+        btn.classList.add('wrong');
+        } else {
+          btn.classList.add('disabled');  
+        }
     });
 
     confirmBtn.textContent = "다음 문제로 이동";
     
     // 해설쪽으로 부드럽게 스크롤
     setTimeout(() => {
-        explanationArea.scrollIntoView({ behavior: 'smooth' });
+        explanationSection.scrollIntoView({ behavior: 'smooth' });
     }, 100);
 };
 
